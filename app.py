fe129488bc62d7d911f07401c4d0a3c77cdef235
KJ_Huang <33456576+KJ-Huang-sz@users.noreply.github.com>
2024-01-16 15:55:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,12 @@
 import gradio as gr
 
-# 输入name字符串，输出Hello {name}!字符串
-def greet(name):
-    return "Hello " + name + "!"
-import time
-import gradio as gr
+def greet(name, intensity):
+    return "Hello " * intensity + name + "!"
 
-def slow_echo(message, history):
-    for i in range(len(message)):
-        time.sleep(0.3)
-        yield "You typed: " + message[: i+1]
-
-gr.ChatInterface(slow_echo).launch()
 demo = gr.Interface(
     fn=greet,
-    inputs=gr.Textbox(lines=2, placeholder="Name Here..."),
-    outputs="text",
-    allow_flagging="never",
+    inputs=["text", "slider"],
+    outputs=["text"],
 )
-if __name__ == "__main__":
-    demo.launch()
+
+demo.launch()
